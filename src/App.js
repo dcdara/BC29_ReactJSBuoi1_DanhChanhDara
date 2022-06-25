1cb25a803b3bad8ecebfd12d0deafdfa4527a4b5
dcdara <danhutnhi@gmail.com>
2022-06-25 10:38:48 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Body from './BaitapLayoutComponent/BaitapThuchanhLayout/Body';
+import Footer from './BaitapLayoutComponent/BaitapThuchanhLayout/Footer';
+import Header from './BaitapLayoutComponent/BaitapThuchanhLayout/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
